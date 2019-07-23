a59fd128e7e07dfd791e9244743f4b3a14a82c45
AMAC71 <mcconnellxca@gmail.com>
2019-07-22 21:48:29 -0400

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Shitty Todo
-
-I wanted to tackle a smaller project in a couple hours so I chose to make a shitty todo app like everyone else. I still had fun making it and wanted to learn about the styled-components package.
---
+++ precache-manifest.9aa10397aa11c279b40858f5415e2aae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907a456582926972378570b9dbbc60d2",
+    "url": "/shitty-todo/index.html"
+  },
+  {
+    "revision": "6c3339cc185e3343bfcb",
+    "url": "/shitty-todo/static/js/2.3e8c0afe.chunk.js"
+  },
+  {
+    "revision": "ea57cacb9665dab32b69",
+    "url": "/shitty-todo/static/js/main.70a6cc0b.chunk.js"
+  },
+  {
+    "revision": "3a1ce6afcd4fa884be65",
+    "url": "/shitty-todo/static/js/runtime~main.0f99d884.js"
+  }
+]);
